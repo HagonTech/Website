f6a842b0c30b63ce9ddd923ec386cc0cac3a71ac
Les <lbaerwaldt1@gmail.com>
2026-08-07 05:54:27 -0500

    publish complete website

---
+++ site.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('.nav-toggle');
+const nav = document.querySelector('#primary-nav');
+if (button && nav) {
+  button.addEventListener('click', () => {
+    const expanded = button.getAttribute('aria-expanded') === 'true';
+    button.setAttribute('aria-expanded', String(!expanded));
+    nav.classList.toggle('is-open', !expanded);
+  });
+}
